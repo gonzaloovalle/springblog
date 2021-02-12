be6c6c81abf0c0b981d537607e8634cb5135622a
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-02-12 17:00:01 -0600

    working on edit and delete

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,11 +1,10 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,16 +12,16 @@ import java.util.List;
 @Controller
 public class PostController {
 
+    private final PostRepository postsDao;
+
+    public PostController(PostRepository postsDao) {
+        this.postsDao = postsDao;
+    }
+
     @GetMapping("/posts")
     public String postIndex(Model model) {
-        Post post1 = new Post("First Post", "This is my first post", 1);
-        Post post2 = new Post("Second Post", "This is my second post", 2);
-        Post post3 = new Post("Third Post", "This is my third post", 3);
 
-        List<Post> postList = new ArrayList<>();
-        postList.add(post1);
-        postList.add(post2);
-        postList.add(post3);
+        List<Post> postList = postsDao.findAll();
 
         model.addAttribute("title", "All Posts");
         model.addAttribute("posts", postList);
@@ -31,11 +30,26 @@ public class PostController {
     }
 
     @GetMapping("/posts/{id}")
-    public String postView(Model model) {
-        Post post = new Post("First Post", "This is my first post", 1);
-        model.addAttribute("title", "Single Posts");
-        model.addAttribute("post", post);
+    public String postView(Model model, @PathVariable long id ) {
+        Post post = postsDao.getOne(id);
         return "posts/show";
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    public String viewEditPostForm(@PathVariable long id, Model model) {
+        model.addAttribute("post", postsDao.getOne(id));
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
+        Post post = new Post(
+                id,
+                title,
+                body
+        );
+        postsDao.save(post);
+        return "redirect:/posts";
     }
 
     @GetMapping("/posts/create")
